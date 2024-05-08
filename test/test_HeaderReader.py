1b8fbe5a94ac4afafb39ed65db066c2c48f64a07
p111787 <olivier.chabot.2@ens.etsmtl.ca>
2024-05-08 13:04:21 -0400

    Added test for Header Data iterator

--- test/test_HeaderReader.py
+++ test/test_HeaderReader.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import numpy as np
 
 from amr_kitchen import HeaderData
 
@@ -21,5 +22,25 @@ class TestSliceData(unittest.TestCase):
             self.assertEqual(hdr.ndims, 3)
             self.assertEqual(hdr.limit_level, Lv)
 
-
-
+    def test_bybinfile_iterator2d(self):
+        hdr = HeaderData(self.pfile2d)
+        for lv in range(hdr.limit_level + 1):
+            for bfname, offsets, indexes in hdr.bybinfile(lv):
+                with open(bfname) as bf:
+                    for idx, ofs in zip(indexes, offsets):
+                        shape = [idx[1][i] - idx[0][i] + 1 for i in range(hdr.ndims)]
+                        shape.append(len(hdr.fields))
+                        bf.seek(ofs)
+                        arr = np.fromfile(bf, 'float64', np.prod(shape))
+                        
+    def test_bybinfile_iterator3d(self):
+        hdr = HeaderData(self.pfile3d)
+        for lv in range(hdr.limit_level + 1):
+            for bfname, offsets, indexes in hdr.bybinfile(lv):
+                with open(bfname) as bf:
+                    for idx, ofs in zip(indexes, offsets):
+                        shape = [idx[1][i] - idx[0][i] + 1 for i in range(hdr.ndims)]
+                        shape.append(len(hdr.fields))
+                        bf.seek(ofs)
+                        arr = np.fromfile(bf, 'float64', np.prod(shape))
+                    
