1b081c0398cf72d9243eb98e61a0cf2fccad16a7
p111787 <olivier.chabot.2@ens.etsmtl.ca>
2024-06-06 21:30:45 -0400

    Added BadTaste Errors to be more specific

--- amr_kitchen/utils.py
+++ amr_kitchen/utils.py
@@ -3,7 +3,12 @@ class TastesBadError(Exception):
         self.value = value
     def __str__(self):
         return repr(self.value)
-    
 
+# TODO: replace with more specific errors
+# depending on where the problem is
+class BadTastingHeadersError(Exception):
+    pass
 
+class BadTastingBinariesError(Exception):
+    pass                    
 
