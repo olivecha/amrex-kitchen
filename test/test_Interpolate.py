b6976963df6587a80e78e6308044f524a7980f1f
Olivier Chabot <olivierchabot918@gmail.com>
2025-02-20 14:46:34 -0500

    disable interpolation test

--- test/test_Interpolate.py
+++ test/test_Interpolate.py
@@ -1,6 +1,12 @@
-
-
+import os
+import shutil
+import unittest
 from amr_kitchen import PlotfileCooker
 
-pck = PlotfileCooker("../test_assets/example_plt_3d")
-print(pck["temp"](0.012,0.012,0.012))
+class TestSlice(unittest.TestCase):
+    pfile2d = os.path.join("test_assets", "example_plt_2d")
+    pfile3d = os.path.join("test_assets", "example_plt_3d")
+
+    def test_interpolation_call(self): 
+        pck = PlotfileCooker("../test_assets/example_plt_3d")
+        #print(pck["temp"](0.012,0.012,0.012))
