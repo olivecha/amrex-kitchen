0dc902bcbbb6660ac4b73466e9fa6c82ade3232b
Olivier Chabot <olivierchabot918@gmail.com>
2025-02-20 14:49:26 -0500

    disable test for real this time

--- test/test_Interpolate.py
+++ test/test_Interpolate.py
@@ -7,6 +7,7 @@ class TestSlice(unittest.TestCase):
     pfile2d = os.path.join("test_assets", "example_plt_2d")
     pfile3d = os.path.join("test_assets", "example_plt_3d")
 
+    @unittest.skip("Not implemented yet")
     def test_interpolation_call(self): 
-        pck = PlotfileCooker("../test_assets/example_plt_3d")
-        #print(pck["temp"](0.012,0.012,0.012))
+        pck = PlotfileCooker(self.pfile3d)
+        print(pck["temp"](0.012,0.012,0.012))
